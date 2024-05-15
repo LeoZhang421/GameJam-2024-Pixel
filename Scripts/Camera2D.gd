12aeae1bca854042252410e4cf7745661fdd9e4a
LeoZhang421 <zyn000421@gmail.com>
2024-05-15 23:43:44 +0800

    fix camera

---
+++ Scripts/Camera2D.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+var dead_zone = 100
+
+# Called when the node enters the scene tree for the first time.
+func _input(event):
+	if event is InputEventMouseMotion:
+		var target = event.position - get_viewport().size * 0.5
+		if target.length() < dead_zone:
+			self.position = Vector2.ZERO
+		else:
+			self.position = target.normalized() * (target.length() - dead_zone) * 0.5
+
+
+
