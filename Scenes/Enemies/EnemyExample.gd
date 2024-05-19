dfb647082f4485d191358c721a6d2bffa1ea12ec
Yujian Xiong <xyj.114514@gmail.com>
2024-05-19 00:59:30 -0400

    敌人寻路逻辑

--- Scenes/Enemies/EnemyExample.gd
+++ Scenes/Enemies/EnemyExample.gd
@@ -8,7 +8,6 @@ class_name Enemy extends Area2D
 @export_range(0, 600, 60) var attack_range: int = 3 * 60 # 60像素的倍数
 @export_range(0.0, 0.5, 1.0) var collide_damage: float = 0.5 #撞击时造成多少倍当前hp的伤害
 @export var start_location: Vector2 = Vector2(500,500)
-@export var end_location: Vector2 = Vector2(1000,500)
 
 # inner variables
 var hp: int = 0
@@ -19,6 +18,8 @@ var target: Area2D = null
 var target_backup: Array[Area2D] = []
 var attacking: bool = false
 var moving: bool = true
+var move_array: Array
+var current_index: int
 
 # onready node variables
 @onready var area_attack_shape = %AreaAttackShape
@@ -30,28 +31,32 @@ signal died
 # basic functions
 func _ready():
 	hp = max_hp
-	distance = start_location.distance_to(end_location)
 	$AnimatedSprite2D.play()
 	$HealthBar.max_value = max_hp
 	_update_health()
+	position = start_location
 	
 	target = null
 	attacking = false
 	$AttackTimer.wait_time = 1.0 / attack_speed
 	$AttackTimer.start()
+	$RerouteTimer.start()
 	area_attack_shape.shape.radius = attack_range
 
 func _process(delta):
-	if moving:
-		if direction:
-			movement += delta * move_speed
-			if movement >= distance:
-				direction = false
+	if moving and move_array:
+		if current_index >= move_array.size()-1:
+			moving = false
+			move_array = []
 		else:
-			movement -= delta * move_speed
-			if movement <= 0:
-				direction = true
-		position = start_location * (movement/distance) + end_location * (1-movement/distance)
+			movement += delta * move_speed
+			var distance = move_array[current_index].distance_to(move_array[current_index+1])
+			if movement < distance:
+				position = move_array[current_index] * (1-movement/distance) + move_array[current_index+1] * (movement/distance)
+			else:
+				movement = 0
+				current_index = current_index + 1
+				position = move_array[current_index]
 
 
 # inner functions
@@ -80,7 +85,7 @@ func set_target() -> void:
 			return
 		else:
 			if target_backup:
-				target = find_closest()
+				target = find_closest(target_backup)
 				return
 			else:
 				target = null
@@ -88,7 +93,7 @@ func set_target() -> void:
 				return
 	else:
 		if target_backup:
-			target = find_closest()
+			target = find_closest(target_backup)
 			attacking = true
 			return
 		else:
@@ -96,11 +101,10 @@ func set_target() -> void:
 			attacking = false
 			return
 
-func find_closest() -> Area2D:
-	assert(target_backup)
-	var min_enemy:Area2D = target_backup[0]
+func find_closest(target_list) -> Area2D:
+	var min_enemy:Area2D = target_list[0]
 	var min_dist = min_enemy.position.distance_to(position)
-	for enemy in target_backup:
+	for enemy in target_list:
 		if enemy.position.distance_to(position) <= min_dist:
 			min_dist = enemy.position.distance_to(position)
 			min_enemy = enemy
@@ -142,3 +146,28 @@ func _on_attack_timer_timeout():
 		attack_event()
 	else:
 		pass
+
+
+func _on_reroute_timer_timeout():
+	var main = get_node("/root/Main")
+	if main.get_node("MerchantLayer").get_child_count() >= 2:
+		var merchant_list = main.get_node("MerchantLayer").get_children()
+		var close = find_closest(merchant_list.slice(1))
+		print("Finding way to merchants: from ", position, " to ", close.position)
+		var temp = main.pathfinder.find_path(position, close.position)
+		if temp:
+			move_array = temp
+			moving = true
+	else:
+		var harbour_list = main.pathfinder.get_harbour_position()
+		var min_harbour:Vector2 = harbour_list[0]
+		var min_dist = min_harbour.distance_to(position)
+		for harbour in harbour_list:
+			if harbour.distance_to(position) <= min_dist:
+				min_dist = harbour.distance_to(position)
+				min_harbour = harbour
+		print("Finding way to harbour: from ", position, " to ", min_harbour)
+		var temp = main.pathfinder.find_path(position, min_harbour)
+		if temp:
+			move_array = temp
+			moving = true
